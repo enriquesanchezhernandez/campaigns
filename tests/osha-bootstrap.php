b49add6c800d5874d72b7a54e3dd4ce7eb261937
Cristian Romanescu <cristian@romanescu.ro>
2014-12-18 11:49:06 +0200

    Fixed global languages variable

--- tests/osha-bootstrap.php
+++ tests/osha-bootstrap.php
@@ -8,13 +8,14 @@ class OshaWebTestCase extends DrupalWebTestCase {
    * {@inheritdoc}
    */
   public function setUp() {
-
     $this->cookieFile = '/tmp/cookie.txt';
     $this->cookies = array();
     parent::setUp();
 
-    global $language, $language_url;
-    $language_url = $language = i18n_language_load('en');
+    global $language, $language_url, $language_content;
+    $language_content = $language_url = $language = i18n_language_load('en');
+
+    $this->cleanup();
   }
 
   /**
