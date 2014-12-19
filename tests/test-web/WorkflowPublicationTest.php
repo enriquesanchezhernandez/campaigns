f7c8ce53cc9d89d351cd3ce9f097e6ac82a09b47
Cristian Romanescu <cristian@romanescu.ro>
2014-12-19 17:41:55 +0200

    Testing project manager assignment screen

--- tests/test-web/WorkflowPublicationTest.php
+++ tests/test-web/WorkflowPublicationTest.php
@@ -2,7 +2,28 @@
 
 class WorkflowPublicationTest extends OshaWebTestCase {
 
-  public function testCreateDraftNode() {
-    //
+  /**
+   * Test access to the project managers configuration per section.
+   */
+  public function testProjectManagersScreen() {
+    $this->loginAsAdmin();
+
+    $pms = osha_workflow_access_pm_get_pm('main-menu', 'menu');
+    if (!empty($pms)) {
+      reset($pms);
+      $pm = current($pms);
+      $this->drupalGet('admin/config/workbench/access/managers');
+      $this->assertText('Main menu');
+      $options = array( "users[{$pm->uid}][remove]" => 1 );
+      $this->drupalPost('admin/config/workbench/access/managers/menu/main-menu', $options, t('Update Project Manager'));
+      $this->assertText('No active project managers have been found.');
+    }
+
+    $pm2 = user_load_by_name('project_manager2');
+    $options = array("add_user" => $pm2->uid);
+    $this->drupalPost('admin/config/workbench/access/managers/menu/main-menu', $options, t('Update Project Manager'));
+
+    $this->drupalGet('admin/config/workbench/access/managers/menu/main-menu');
+    $this->assertText('project_manager2');
   }
 }
