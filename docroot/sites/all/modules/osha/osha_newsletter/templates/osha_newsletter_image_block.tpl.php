1fd2cbee1e85a23cb56a392063b2ef60dab0bbba
Andrei Melis <andrei.melis@eaudeweb.ro>
2015-02-18 10:59:48 +0200

    CW-1392 Moved background image

--- docroot/sites/all/modules/osha/osha_newsletter/templates/osha_newsletter_image_block.tpl.php
+++ docroot/sites/all/modules/osha/osha_newsletter/templates/osha_newsletter_image_block.tpl.php
@@ -2,13 +2,13 @@
 	<?php if($background_image_url != NULL) {
 		print theme('image', array(
 			'path' => $background_image_url,
-			'alt' => 'Join OSHMail Subscribers',
+			'alt' => 'Join OSHMail subscribers',
 		));
 	} else {
 		$theme_path = drupal_get_path('theme', variable_get('theme_default', 'osha_frontend'));
 		print theme('image', array(
-			'path' => $theme_path . '/images/join_65000_subscribers.png',
-			'alt' => 'Join OSHMail Subscribers',
+			'path' => $theme_path . '/images/content/blocks/join_65000_subscribers.png',
+			'alt' => 'Join OSHMail subscribers',
 		));
 	}?>
 	<div id="block_newsletter_image_text">
