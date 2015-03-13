68a221c133e4493867fd6323780e770d64f04c2d
Cristian Romanescu <cristian@romanescu.ro>
2015-03-13 20:47:23 +0200

    Fixes CW-1573 - Show related wiki articles on infographics page

--- docroot/sites/all/modules/osha/osha_blocks/templates/oshwiki_featured_articles.tpl.php
+++ docroot/sites/all/modules/osha/osha_blocks/templates/oshwiki_featured_articles.tpl.php
@@ -1,16 +1,9 @@
+<?php if (!empty($tagged_wikis)): ?>
+<div id="related-wiki" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 <?php
-  $node = menu_get_object();
-  if (isset($node)) {
-    if ($node->type == 'publication' || ($node->type == 'article' && !empty($node->article_type_code) && $node->article_type_code == 'section')) {
-      if (!empty($tagged_wikis)) {?>
-  <div id="related-wiki" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-      <?php
-        foreach ($tagged_wikis as $wiki) {
-          print render($wiki);
-        }?>
-  </div>
-      <?php
-      }
-    }
+  foreach ($tagged_wikis as $wiki) {
+    print render($wiki);
   }
 ?>
+</div>
+<?php endif; ?>
