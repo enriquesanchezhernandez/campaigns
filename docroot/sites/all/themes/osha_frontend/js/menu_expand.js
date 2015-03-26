1bdda98d5f041aaa3d7e4f5420f04175c3f85998
Radu Ciofringeanu <radu.sc@gmail.com>
2015-03-26 15:33:46 +0200

    cw-1597 Expand/Collapsa Menu js

--- docroot/sites/all/themes/osha_frontend/js/menu_expand.js
+++ docroot/sites/all/themes/osha_frontend/js/menu_expand.js
@@ -1,10 +1,22 @@
 /* Expand vertical (left, right) menu on click */
 jQuery(document).ready(function() {
-    jQuery(function(){
-        jQuery('div#block-menu-block-2>ul#menu>li').hoverIntent(function(){
-            jQuery(this).find('>ul').fadeIn('fast');
-        },function(){
-            jQuery(this).find('>ul').fadeOut('fast');
-        });
+    jQuery('div#block-menu-block-2').find('a.menu__link').each(function(i) {
+        if (jQuery(this).parent().hasClass('is-expanded') && !jQuery(this).parent().hasClass('is-active-trail')){
+            // Hide ul list
+            jQuery(this).next('ul#main-menu-links').hide();
+            // Add a span after expandable link
+            jQuery(this).after("<span class='expand_menu'> + </span>");
+
+            // Expand/collapse menu
+            jQuery(this).next('span').click(function(){
+                jQuery(this).next('ul#main-menu-links').slideToggle();
+                // Add remove css class for expanded/collapsed menu
+                if (jQuery(this).hasClass('expanded')) {
+                    jQuery(this).removeClass('expanded');
+                }else {
+                    jQuery(this).addClass('expanded');
+                }
+            });
+        }
     });
 });
