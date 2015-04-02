4625e199e41858f195b48721bb1169571115c10e
Cristian Romanescu <cristian@romanescu.ro>
2015-04-02 10:27:52 +0300

    More tweakings to Vanrnish setup

--- varnish.sh
+++ varnish.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-varnishd -d -a :8081 \
-  -f ./conf/varnish-devel.vcl \
-  -s malloc,1024M \
+sudo varnishd -F -a :80 \
+  -f /Users/cristiroma/Work/osha/project/conf/varnish-devel.vcl \
+  -s malloc,256M \
   -T localhost:6082 \
   -p sess_workspace=262144 \
   -p log_hashstring=off \
@@ -11,7 +11,6 @@ varnishd -d -a :8081 \
   -p thread_pools=4 \
   -p thread_pool_min=100 \
   -p esi_syntax=1 \
-#  -p thread_pool_add_delay=2 \
-#  -p thread_pool_max=4000 \
-#  -p session_linger=50 \
-
+  -p thread_pool_add_delay=2 \
+  -p thread_pool_max=4000 \
+  -p session_linger=50 \
