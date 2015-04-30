22c37ebaa5c22ea96fab3ab3cb248335fadff6e5
Andrei Melis <andrei.melis@eaudeweb.ro>
2015-04-30 19:15:06 +0300

    Fixed bat file for install

--- update_from_staging.bat
+++ update_from_staging.bat
@@ -3,19 +3,14 @@
 rem Go to docroot/
 cd docroot/
 
+call drush sql-drop -y
+
 rem Sync from edw staging
 call drush downsync_sql @osha.staging.sync @osha.local -y -v
-
-rem Build the site
-call drush osha_build -y
 
 rem Devify - development settings
 call drush devify --yes
 call drush devify_solr
 
-call drush cc all
-
-rem echo "Running cron to cleanup ..."
-rem call drush cron
-
-call drush cc all
+rem Build the site
+call drush osha_build -y
