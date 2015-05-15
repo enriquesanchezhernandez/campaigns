a49bfffdd5f18b3137181d120003b3ad2bf66f7e
Cristian Romanescu <cristian@romanescu.ro>
2015-05-15 19:35:58 +0300

    HCW Preliminary spring cleanup

--- docroot/sites/all/modules/osha/osha_blocks/templates/oshwiki_featured_articles.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php if (!empty($tagged_wikis)): ?>
-<div id="related-wiki" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-<?php
-  foreach ($tagged_wikis as $wiki) {
-    print render($wiki);
-  }
-?>
-</div>
-<?php endif; ?>
