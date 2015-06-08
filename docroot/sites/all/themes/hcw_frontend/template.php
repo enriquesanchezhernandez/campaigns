ae1397936191d524e6e5e7c1bda9128e1b585e0f
Cristian Romanescu <cristian@romanescu.ro>
2015-06-09 01:35:28 +0300

    Fixed footer menu

--- docroot/sites/all/themes/hcw_frontend/template.php
+++ docroot/sites/all/themes/hcw_frontend/template.php
@@ -1,1 +1,9 @@
 <?php
+
+
+/**
+ * Render footer menu as nav-pills.
+ */
+function bootstrap_menu_tree__menu_footer_menu(&$variables) {
+  return '<ul class="menu nav nav-pills">' . $variables['tree'] . '</ul>';
+}
