ded970c730e060cd5e60d8949fd3561b75f3004b
Andrei Melis <andrei.melis@eaudeweb.ro>
2015-06-09 17:53:27 +0300

    Removed welcome message and front-page content

--- docroot/sites/all/themes/hcw_frontend/template.php
+++ docroot/sites/all/themes/hcw_frontend/template.php
@@ -7,3 +7,10 @@
 function bootstrap_menu_tree__menu_footer_menu(&$variables) {
   return '<ul class="menu nav nav-pills">' . $variables['tree'] . '</ul>';
 }
+
+function hcw_frontend_preprocess_page(&$vars) {
+    if (drupal_is_front_page()) {
+        unset($vars['page']['content']['system_main']['default_message']);
+        drupal_set_title('');
+    }
+}
