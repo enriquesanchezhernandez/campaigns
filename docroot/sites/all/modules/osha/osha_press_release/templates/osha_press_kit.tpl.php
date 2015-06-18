c155ea3171b5dfd4a9e0afcd44453481b0373200
Dragos Dumitrescu <dumitrescu.dragos1@gmail.com>
2015-06-18 13:08:45 +0300

    Press room listing and node view + blocks

---
+++ docroot/sites/all/modules/osha/osha_press_release/templates/osha_press_kit.tpl.php
@@ -0,0 +1,6 @@
+<div class="osha-press-contact-block-intro-text">
+  <?php print $osha_press_kit_intro_text; ?>
+</div>
+<div class="osha-press-contact-block-link">
+  <?php print $osha_press_kit_link; ?>
+</div>
