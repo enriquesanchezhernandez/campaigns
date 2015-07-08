74c0afeb065d81c3888ba64b2880683a7f8b6c2f
Octavian Belu <belu.octavian@yahoo.com>
2015-07-08 17:43:38 +0300

    Added newsletter blocks config

--- docroot/sites/all/modules/osha/osha_newsletter/templates/osha_newsletter_image_block.tpl.php
+++ docroot/sites/all/modules/osha/osha_newsletter/templates/osha_newsletter_image_block.tpl.php
@@ -2,16 +2,16 @@
 	<?php if($background_image_url != NULL) {
 		print theme('image', array(
 			'path' => $background_image_url,
-			'alt' => 'Join OSHMail subscribers',
+			'alt' => 'Newsletter background',
 		));
 	} else {
-		$theme_path = drupal_get_path('theme', variable_get('theme_default', 'osha_frontend'));
+		$theme_path = drupal_get_path('theme', variable_get('theme_default', 'hwc_frontend'));
 		print theme('image', array(
-			'path' => $theme_path . '/images/content/blocks/join_65000_subscribers.png',
-			'alt' => 'Join OSHMail subscribers',
+			'path' => $theme_path . '/images/block_newsletter/background.jpg',
+			'alt' => 'Newsletter background',
 		));
 	}?>
 	<div id="block_newsletter_image_text">
-		<?php print $start_text . ' ' . $subscribers_no . ' ' . $end_text?>
+		<?php print $intro_text ?>
 	</div>
 </div>
