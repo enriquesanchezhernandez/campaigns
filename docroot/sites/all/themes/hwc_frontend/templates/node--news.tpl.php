28931a4d8cb22706e40962e6d3f277bd1ca14af2
Octavian Belu <belu.octavian@yahoo.com>
2015-07-22 18:20:28 +0300

    HCW-273 Changed the news template for full view mode

---
+++ docroot/sites/all/themes/hwc_frontend/templates/node--news.tpl.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for speakers.
+ */
+?>
+
+  <?php
+  if ($view_mode === 'full') {
+    ?>
+    <h1>News</h1>
+    <?php
+  }
+  // We hide the comments and links now so that we can render them later.
+  hide($content['comments']);
+  hide($content['links']);
+  print render($content);
+  ?>
