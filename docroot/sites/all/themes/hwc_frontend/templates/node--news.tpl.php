857fa176ea1ea1604cd2ab7f707668c4f2b0f0c0
Octavian Belu <belu.octavian@yahoo.com>
2015-07-22 18:26:08 +0300

    Passed the news text to t function

--- docroot/sites/all/themes/hwc_frontend/templates/node--news.tpl.php
+++ docroot/sites/all/themes/hwc_frontend/templates/node--news.tpl.php
@@ -7,9 +7,7 @@
 
   <?php
   if ($view_mode === 'full') {
-    ?>
-    <h1>News</h1>
-    <?php
+    print '<h1>' . t('News') . '</h1>';
   }
   // We hide the comments and links now so that we can render them later.
   hide($content['comments']);
