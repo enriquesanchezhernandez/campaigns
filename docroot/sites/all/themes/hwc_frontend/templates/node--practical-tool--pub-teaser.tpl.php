a2a748f5818874f4abea67b6c6fd4d5c760ba797
Octavian Belu <belu.octavian@yahoo.com>
2015-09-23 13:09:54 +0300

    Access tool link: removed old field formatters and added formatter for practical tools listing

--- docroot/sites/all/themes/hwc_frontend/templates/node--practical-tool--pub-teaser.tpl.php
+++ docroot/sites/all/themes/hwc_frontend/templates/node--practical-tool--pub-teaser.tpl.php
@@ -3,14 +3,7 @@
 $content = $variables['content'];
 $node = $variables['node'];
 $view = node_view($node, 'teaser');
-$q = db_select('languages', 'l');
-$q->fields('l', array('language', 'name', 'native'));
-$q->innerJoin('entity_translation', 'a', 'l.language = a.language');
-$q->condition('a.entity_id', $node->nid);
-$q->condition('a.entity_type', 'node');
-$languages = $q->execute()->fetchAll();
 ?>
 <div class="practical-tool-item">
   <?php print render($view); ?>
-  <?php print theme('hwc_practical_tool_language_list', array('languages' => $languages)); ?>
 </div>
