585be78e246731b07018bad847701c6a925e2ecf
Octavian Belu <belu.octavian@yahoo.com>
2015-09-23 14:15:53 +0300

    Changed the field formatter in panel and added the css

--- docroot/sites/all/modules/hwc/hwc_practical_tool/templates/hwc_practical_tool_access_tool_link_details.tpl.php
+++ docroot/sites/all/modules/hwc/hwc_practical_tool/templates/hwc_practical_tool_access_tool_link_details.tpl.php
@@ -9,7 +9,7 @@ foreach ($items as $item) {
 }
 $entities = entity_load('field_collection_item', $entities_to_load);
 ?>
-<div class="field field-access-tool-link field-type-field-collection access-tool-link field-label-inline clearfix">
+<div class="field field-access-tool-link field-type-field-collection field-label-inline clearfix">
   <div class="field-label"><?php print $label_1; ?>&nbsp;</div>
   <div class="field-items">
     <?php foreach ($entities as $key => $entity): ?>
