ba1cff6fc162cde0e8e9c8c86fc621797f08eca6
Radu Ciofringeanu <radu.sc@gmail.com>
2015-09-29 16:22:16 +0300

    HCW-584 Refactor

--- docroot/sites/all/modules/hwc/hwc/js/hwc_tabs_to_select_list.js
+++ docroot/sites/all/modules/hwc/hwc/js/hwc_tabs_to_select_list.js
@@ -1,22 +1,24 @@
-jQuery(document).ready(function(){
-    var id = '';
-    var name = '';
-    var tab_container = jQuery('#quicktabs-container-view__partners_list__block');
+(function($) {
+    $(function() {
+        var id = '';
+        var name = '';
+        var tab_container = $('div.quicktabs-wrapper div.quicktabs_main');
 
-    jQuery('ul.quicktabs-tabs').addClass('hidden-xs');
-    jQuery('.hwc-alphabet-container').addClass('hidden-xs');
+        $('ul.quicktabs-tabs').addClass('hidden-xs');
+        $('.hwc-alphabet-container').addClass('hidden-xs');
 
-    jQuery('ul.quicktabs-tabs').parent().prepend('<select id="select_from_tabs" class="visible-xs"></select>');
+        $('ul.quicktabs-tabs').parent().prepend('<select class="select_from_tabs visible-xs"></select>');
 
-    jQuery('ul.quicktabs-tabs li').each(function() {
-        id = jQuery(this).find('a').attr('id');
-        name = jQuery(this).find('a').text();
-        jQuery('#select_from_tabs').prepend('<option value="'+id+'">'+name+'</option>');
+        $('ul.quicktabs-tabs li').each(function() {
+            id = $(this).find('a').attr('id');
+            name = $(this).find('a').text();
+            $('.select_from_tabs').prepend('<option value="'+id+'">'+name+'</option>');
+        });
+
+        $('.select_from_tabs').change(function(){
+            var tab = $(this).val().replace('quicktabs-tab','#quicktabs-tabpage');
+            tab_container.find('div.quicktabs-tabpage').addClass('quicktabs-hide');
+            tab_container.find(tab).removeClass('quicktabs-hide');
+        });
     });
-
-    jQuery('#select_from_tabs').change(function(){
-        var tab = jQuery(this).val().replace('quicktabs-tab','#quicktabs-tabpage');
-        tab_container.find('div.quicktabs-tabpage').addClass('quicktabs-hide');
-        tab_container.find(tab).removeClass('quicktabs-hide');
-    });
-})
+})(jQuery);
