aa9bfdfc21dca751620a99e418e569e491c67640
Cristian Romanescu <cristian@romanescu.ro>
2015-10-02 14:51:11 +0300

    Fixed HCW-487 - Months do not flicker - now appear when the year is selected

--- docroot/sites/all/modules/osha/osha_events/includes/js/collapse_block.js
+++ docroot/sites/all/modules/osha/osha_events/includes/js/collapse_block.js
@@ -1,24 +1,14 @@
 jQuery(document).ready(function()
 {
-    // init: collapse all groups
     osha_events_hide_date_filters(1);
-
-    // click event: toggle visibility of group clicked (and update icon)
-    jQuery(".events-filter-by-date h3").click(function()
-    {
-        osha_events_hide_date_filters(0);
-
-        jQuery(this).siblings("ul").slideToggle();
-		jQuery(this).toggleClass("expand");
-    });
+    jQuery('.view-id-events').show();
 });
 
 function osha_events_hide_date_filters(initial_load){
-    jQuery(".events-filter-by-date ul").each(function(i)
-    {
-        if(!initial_load){ // onclick phase
+    jQuery('.events-filter-by-date ul').each(function(i) {
+        if(!initial_load) { // onclick phase
             jQuery(this).hide();
-        }else if(!jQuery(this).find('a.current_month').length && !jQuery(this).parent().find('a.current_year').length){ // init phase
+        } else if(!jQuery(this).find('a.current_month').length && !jQuery(this).parent().find('a.current_year').length) { // init phase
             jQuery(this).hide();
         }
     });
