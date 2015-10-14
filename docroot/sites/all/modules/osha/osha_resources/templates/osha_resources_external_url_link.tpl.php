eee5e96161b1e6efd3d5a28a5be369f1ecf135ea
Claudia Ifrim <ifrim.claudia@gmail.com>
2015-10-14 17:51:52 +0300

    HCW-619 - add image for internal link

--- docroot/sites/all/modules/osha/osha_resources/templates/osha_resources_external_url_link.tpl.php
+++ docroot/sites/all/modules/osha/osha_resources/templates/osha_resources_external_url_link.tpl.php
@@ -1,5 +1,13 @@
 <?php foreach ($items as $item): ?>
   <a href="<?php print $item['url']; ?>" class="external-link">
-    <?php print $item['title']; ?>
+    <?php
+    if (strpos($item['url'],substr($item['title'], 0, -3)) === false) {
+      print $item['title'];
+    } else {
+      $name = isset($entity->title_field[$language->language][0])
+        ? $entity->title_field[$language->language][0]['value']
+        : $entity->title_field['en'][0]['value'];
+      print $name;
+    }?>
   </a>
 <?php endforeach; ?>
