b8fafb5478fafbaa3e34ded908ee0bc4fe1f0120
Claudia Ifrim <ifrim.claudia@gmail.com>
2015-10-28 10:46:08 +0200

    HCW-757 - design changes

--- docroot/sites/all/themes/hwc_frontend/templates/field--field_images--hwc_gallery.tpl.php
+++ docroot/sites/all/themes/hwc_frontend/templates/field--field_images--hwc_gallery.tpl.php
@@ -4,7 +4,7 @@
   <?php endif; ?>
   <div class="field-items"<?php print $content_attributes; ?>>
     <?php foreach ($items as $delta => $item): ?>
-      <div class="field-item col-sm-4 <?php print $delta % 3 ? '' : 'clearfix'; ?>"<?php print $item_attributes[$delta]; ?>><?php print render($item); ?></div>
+      <div class="field-item col-md-4 col-sm-6 col-xs-12 <?php print $delta % 3 ? '' : 'clearfix'; ?>"<?php print $item_attributes[$delta]; ?>><?php print render($item); ?></div>
     <?php endforeach; ?>
   </div>
 </div>
