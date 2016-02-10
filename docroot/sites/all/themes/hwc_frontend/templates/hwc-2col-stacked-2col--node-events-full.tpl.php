e66a52c573fbed3bdc76695da3f6741d59f01b93
dragos-dumi <dumitrescu.dragos1@gmail.com>
2016-02-10 15:54:35 +0200

    HCW-1142 - added submit button for validation on view node

--- docroot/sites/all/themes/hwc_frontend/templates/hwc-2col-stacked-2col--node-events-full.tpl.php
+++ docroot/sites/all/themes/hwc_frontend/templates/hwc-2col-stacked-2col--node-events-full.tpl.php
@@ -1,5 +1,11 @@
 <<?php print $layout_wrapper; print $layout_attributes; ?> class="hwc-2col-stacked-2col <?php print $classes;?> clearfix">
 
+<?php if (!empty($content['submit_content_form'])): ?>
+  <div class="node-submit-validation-form">
+    <?php print drupal_render($content['submit_content_form']); ?>
+  </div>
+<?php endif; ?>
+
 <?php if (isset($title_suffix['contextual_links'])): ?>
   <?php print render($title_suffix['contextual_links']); ?>
 <?php endif; ?>
